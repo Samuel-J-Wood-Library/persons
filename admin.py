e8f339917e2bb49c89228f084b0f5fae1236a3d9
https://oxpeter@bitbucket.org <oxpeter+git@gmail.com>
2019-07-17 20:59:54 -0400

    created basic admin models

--- admin.py
+++ admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+# customize the individual model views:
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('preferred_name',
+                    'first_name', 
+                    'last_name',
+                    'cwid', 
+                    'email_primary',
+                    )
+    search_fields = ('preferred_name',
+                    'first_name', 
+                    'last_name',
+                    'cwid',
+                    )
+
+@admin.register(Department)
+class DeptAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    )
+    search_fields = ('name',
+                    )   
+                    
+@admin.register(Organization)
+class OrgAdmin(admin.ModelAdmin):
+    list_display = ('short_name',
+                    'name'
+                    )
+    search_fields = ('name',
+                    )   
+                    
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    )
+    search_fields = ('name',
+                    )   
